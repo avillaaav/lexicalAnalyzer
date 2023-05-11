5f73a922f8a8735b07a556111485cc8c0e063960
Angel Villa <54606104+avillaaav@users.noreply.github.com>
2023-05-10 17:34:10 -0700

    added

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 class Token:
-    def __init__(self, token_type, lexeme):          # constructor method that is used for our Token class
+    def __init__(self, token_type, lexeme):          # __init__ used to initialize attri
         self.token_type = token_type                 # constructor uses two params, token_type & lexeme
         self.lexeme = lexeme
 
-    def __repr__(self):                              # __repr__ method used for our class as well
+    def __repr__(self):                              # __repr__ method used to return class obj as string
         return f"{self.token_type}\t{self.lexeme}"   # this will output our token types and lexemes
 
-def lexer(input_scode):                              # this is our lexer class that takes in our given source code file
-    # each of the following are our token types, and each one matches to a state
-    q0 = 'initial'
+def lexer(input_scode):                              # organized states
+    q0 = 'initial'                                   # each of the following are our token types, and each one matches to a state
     q1 = 'keyword'
     q2 = 'separator'
     q3 = 'identifier'
@@ -16,122 +15,102 @@ def lexer(input_scode):                              # this is our lexer class t
     q5 = 'real'
     q6 = 'integer'
 
-    # starts out state at q0, everything is empty
-    state = q0
+    state = q0                                      # starts out state at q0, everything is empty
     lexeme = ''                                     # empty string initialized
     tokens = []                                     # tokens list
 
-    def appendToken_resetState():                   # this function appends Lexeme as token, and reset state & lexeme to continue
+    def appendToken_resetState():                   # this function appends lexem & token, and reset state & lexeme to continue
         nonlocal state, lexeme, tokens              # nonlocal variables used to access outside function
 
-        if lexeme == "while":                       # if lexeme = keyword, set to state q1
-            state = q1                              # state = q1
+        if lexeme == "while":                       # if lexeme = keyword, set to state q1 - ONLY WORKS FOR WHILE
+            state = q1
 
         tokens.append(Token(state, lexeme))         # state and lexeme added to list (output)
 
-        # reset state and current lexeme back to start
-        state = q0
+        state = q0                                  # reset state and current lexeme back to start
         lexeme = ''
 
-    # we will use an index to go through every char in source code, so set index = 0
-    i = 0
+    i = 0                                           # we will use an index to go through every char in source code, so set index = 0
 
 # TOKEN RECOGNIZER
 # ________________________________________________________________________________________________________________
     while i < len(input_scode):
-        # assign char at index 'i' to the char variable, we will use this to iterate
-        char = input_scode[i]
+        char = input_scode[i]                       # assign char at index 'i' to the char variable, we will use this to iterate
 
-        # when initial state, determine next state
         if state == q0:
-            # if character is blank, +1 to i and iterate to next i
-            if char.isspace():
+            if char.isspace():                      # USED TO IGNORE SPACES IN INPUT
                 i += 1
                 continue
 
-            # if the char is letter or underscore, set state to q3 , and append lexeme
-            elif char.isalpha() or char == '_':
+            elif char.isalpha() or char == '_':     # if the char is letter or underscore, set state to q3, and append
                 state = q3
                 lexeme += char
 
-            # if the char is a numeral digit, change state to q6, and append the lexeme to the list
-            elif char.isdigit():
+            elif char.isdigit():                    # if the char is a numerical digit, change state to q6, append to list
                 state = q6
                 lexeme += char
 
-            # if the char is a seperator such as ()[]{}, set state to q2, and append lexeme to list
-            elif char in "([{;)]}":
+            elif char in "([{;)]}":                 # if the char is a seperator such as ()[]{}, set state to q2, append to list
                 state = q2
                 lexeme += char
 
-            # if the char is operator such as given below, set state to q4, and append lexeme to list
-            elif char in "<>=!+-*/%":
+            elif char in "<>=!+-*/%":               # if the char is operator such as given below, set state to q4, append to list
                 state = q4
                 lexeme += char
 
-            # if the char is real decimal number, set state to q5, and append lexeme to list
-            elif char == '.':
+            elif char == '.':                       # if the char is real decimal number, set state to q5, append to list
                 state = q5
                 lexeme += char
 
 # STATE RECOGNIZER
 # ________________________________________________________________________________________________________________
-        # if state is not in initial state q0
         else:
-            # if in state 3
             if state == q3:
-                # if the char is numeric or underscore, append it to lexeme
-                if char.isalnum() or char == '_':
+                if char.isalpha() or char == '_':  # if the char is numeric or underscore, append it
                     lexeme += char
-                # else, reset and run function appendToken_resetState
-                else:
+
+                else:                              # else, reset and run function appendToken_resetState - ADDS TO OUTPUT
                     appendToken_resetState()
                     continue
 
-            # if state = q6
             elif state == q6:
-                # if char is a digit, append to lexeme
-                if char.isdigit():
+                if char.isdigit():                 # check for more digits
                     lexeme += char
-                # if the char is a period, transition to real state q5 & append char to list
-                elif char == '.':
+
+                elif char == '.':                  # if the char is a period, this defines it as real number
                     state = q5
                     lexeme += char
-                # else, reset and run function appendToken_resetState
-                else:
+
+                else:                              # run function appendToken_resetState which adds to output
                     appendToken_resetState()
                     continue
 
-            # if state = q5
-            elif state == q5:
-                # if the char is number, append to table
-                if char.isdigit():
+            elif state == q5:                      # if the char is number, append to table
+
+                if char.isdigit():                 # check for more numbers IN the number
                     lexeme += char
-                # else, reset and run function appendToken_resetState
+
                 else:
-                    appendToken_resetState()
+                    appendToken_resetState()       # add to output
                     continue
 
-            # if state is q4
             elif state == q4:
-                # if char is one of the operators, add to list
-                if char in "<>=!":
-                    lexeme += char
-                # else, reset and run function appendToken_resetState
-                else:
+                if char in "<>=!":                 # if char is one of the operators, add to list
+                    lexeme += char                 # check if << >> == <= >= etc.
+
+                else:                              # run function appendToken_resetState - ADD TO OUTPUT
                     appendToken_resetState()
                     continue
 
-            # if state = q2
             elif state == q2:
-                # create a token & reset state lexeme
+                # ADD TO OUTPUT
                 appendToken_resetState()
                 continue
 # ________________________________________________________________________________________________________________
         # increment index 'i', move to next character in index_scode.txt
         i += 1
 
-    # add final token if needed
+    # add final token to output once the code is done iterating
     if state != q0:
         appendToken_resetState()
 
@@ -147,7 +126,7 @@ def main():
 
     # write table into file
     with open('output', 'w') as output_file:
-        # Write the header
+        # write the header
         output_file.write("TOKEN\t----\tLEXEME\n")
         output_file.write("______________________\n")
 
